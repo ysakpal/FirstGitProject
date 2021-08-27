2bc39ab701b9f544ad667df6c7add1f0eeed7341
White Ghost <ysakpal20@gmail.com>
2021-08-27 15:43:38 +0530

    git .swp dlt

---
+++ fileyash.py
@@ -0,0 +1,6 @@
+
+print("hello yash long time no see")
+print("is it all good ")
+
+
+
--- hello_world.py
+++ hello_world.py
@@ -3,3 +3,6 @@ print("------------")
 print("good morning  mumbai")
 #all good as all
 #helth is welth
+print("how are you doing")
+
+
